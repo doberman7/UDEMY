94d10d0528ce5d4bf3dd6d11a4c8f80e3550c440
doberman7 <ivanrubioangeles@gmail.com>
2017-06-24 14:23:40 +0800

    error callback funtion al final del JSON request, funcional

--- intro_jquery/minicourse-ajax-project/js/script.js
+++ intro_jquery/minicourse-ajax-project/js/script.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
 
-
-
   function loadData() {
       var $body = $('body');
       var $wikiElem = $('#wikipedia-links');
@@ -17,7 +15,7 @@ $(document).ready(function(){
       var cityStr = $("#city").val();
       var address = streetStr + ", " + cityStr;
 
-      $greeting.text('So you wanto to live at '+ address + "?").delay(8000000000000000);
+      $greeting.text('So you wanto to live at '+ address + "?");
                         //Deberia funcionar:
                         //https://maps.googleapis.com/maps/api/streetview?size=600x300&location=24 willie mays plaza, san francisco, ca&key=AIzaSyDpNKawxZsx5OBc_vGKjMA4Ny3FlBZomQM
                         //https://maps.googleapis.com/maps/api/streetview?size=600x300&location=24%20willie%20mays%20plaza,%20san%20francisco,%20ca&key=AIzaSyDpNKawxZsx5OBc_vGKjMA4Ny3FlBZomQM
@@ -26,11 +24,14 @@ $(document).ready(function(){
       $body.append('<img class="bgimg" src="'+streetViewURL+'">');
 
       //asignar api nytimes concatenar la variable "ciudad" y  concatenar  la llave de la API
-      var nytimesURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q='+cityStr+'&api-key=30f3bcd3905d40a596982ef54001764c'
+
+      var nytimesURL = 'https://api.nyAtimes.com/svc/search/v2/articlesearch.json?q='+cityStr+'&api-key=30f3bcd3905d40a596982ef54001764c'
 
 
-      //SOLICITAR JSON c
+
+      //SOLICITAR JSON---------------------
       $.getJSON(nytimesURL , function( data ){
+
         //mostrat texto "Articulos", concatenar variable cityStr
         $nytHeaderElem.text('Articulos'+cityStr);
 
@@ -41,17 +42,17 @@ $(document).ready(function(){
         //iterar en cada articulo,
         $.each(articles, function(index, article) {
           //anexar item de lista, concatenar un lick con el url, el headline y un snippet
+
           $nytElem.append('<li class="article">'+'<a href="'+article.web_url+'">'+article.headline.main+'</a>' + '<p>'+article.snippet+'</p>'+'</li>');
         });
+        //An alternative construct to the error callback option, the .fail() method replaces the deprecated .error() method.
+      }).fail("error", function error() {
+        alert( "ALgo salio mal" );
+      });//Fin de JSON request
 
-        //Opcion alternano provada aún
-        // for (var i = 0; i < articles.length; i++){
-        //   var article =article[i];
-        //$nytElem.append('<li class="article">'+'<a href="'+article.web.url+'">'+article.headline.main+'</a>' + '<p>'+article.snippet+'</p>'+'</li>');
-        // };
-
-      });
       return false;
   };
+
   $('#form-container').submit(loadData);
+
 });
